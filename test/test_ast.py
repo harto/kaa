bf5f75f7fe0f1ad8177f6ecd1b8bb3bc995cfaaa
Stuart Campbell <stuart@harto.org>
2016-02-20 15:31:19 -0800

    Cleanup imports

--- test/test_ast.py
+++ test/test_ast.py
@@ -1,39 +1,39 @@
-from kaa import ast
+from kaa.ast import Func, List, Symbol, UnboundSymbolException, Value
 from unittest import TestCase
 
 class FuncTest(TestCase):
 
     def test_call_proxies_to_python_function(self):
-        add = ast.Func(lambda x, y: x + y)
+        add = Func(lambda x, y: x + y)
         self.assertEqual(5, add(2, 3))
 
 class ListTest(TestCase):
 
     def test_empty_list_evaluates_to_itself(self):
-        l = ast.List()
+        l = List()
         self.assertEqual(l, l.eval({}))
 
     def test_non_empty_list_evaluates_to_function_call(self):
-        l = ast.List([ast.Func(lambda x: "Hello, %s" % x), ast.Value("world")])
+        l = List([Func(lambda x: "Hello, %s" % x), Value("world")])
         self.assertEqual("Hello, world", l.eval({}))
 
 class SymbolTest(TestCase):
 
     def test_symbol_evaluates_to_bound_value(self):
-        s = ast.Symbol('foo')
+        s = Symbol('foo')
         self.assertEqual(42, s.eval({'foo': 42}))
 
     def test_unbound_symbol_raises_error(self):
-        s = ast.Symbol('foo')
-        self.assertRaises(ast.UnboundSymbolException,
+        s = Symbol('foo')
+        self.assertRaises(UnboundSymbolException,
                           lambda: s.eval({}))
 
     def test_equality(self):
-        self.assertEqual(ast.Symbol('foo'), ast.Symbol('foo'))
-        self.assertNotEqual(ast.Symbol('bar'), ast.Symbol('foo'))
+        self.assertEqual(Symbol('foo'), Symbol('foo'))
+        self.assertNotEqual(Symbol('bar'), Symbol('foo'))
 
 class ValueTest(TestCase):
 
     def test_equality(self):
-        self.assertEqual(ast.Value(42), ast.Value(42))
-        self.assertNotEqual(ast.Value(42.0), ast.Value(42))
+        self.assertEqual(Value(42), Value(42))
+        self.assertNotEqual(Value(42.0), Value(42))
