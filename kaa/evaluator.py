ad321cdbf87a594ee046648b123dcf652387bc78
Stuart Campbell <stuart@harto.org>
2016-02-27 21:24:23 -0800

    Allow evaluation of anything

--- kaa/evaluator.py
+++ kaa/evaluator.py
@@ -2,21 +2,10 @@ def eval(expr, ns):
     try:
         return expr.eval(ns)
     except AttributeError:
-        pass
-
-    if is_valid_native_type(expr):
         return expr
-
-    raise UnevaluatableTypeException(type(expr))
 
 def eval_all(exprs, ns):
     result = None
     for e in exprs:
         result = eval(e, ns)
         return result
-
-def is_valid_native_type(expr):
-    return type(expr) in (int, str) \
-        or callable(expr)
-
-class UnevaluatableTypeException(Exception): pass
